8e5e5584e85721a128981b9f7702d8f34330743d
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-04-11 17:12:25 -0300

    Create WeatherCard.tsx

---
+++ src/components/WeatherCard.tsx
@@ -0,0 +1,16 @@
+import { WeatherCardProps } from "../types/WeatherAPIResponse";
+
+export default function WeatherCard({weatherImg, atmosphere ,temperature, location}: WeatherCardProps) {
+   return (
+      <>
+         <div className="flex flex-col gap-4 items-center justify-center">
+            <p className="text-white font-semibold text-lg">{location}</p>
+            <img className="w-30" src={weatherImg} alt="Current weather" />
+            <h3 className="relative right-2 text-5xl text-white font-bold">
+               {temperature == "not found"? ' ': Number(temperature).toFixed(0)} <span className="absolute top-0 left-14 text-xl font-normal">{temperature == "not found"? " " : '°C'}</span>
+            </h3>
+            <p className="text-lg text-white">{atmosphere}</p>
+         </div>
+      </>
+   );
+}
